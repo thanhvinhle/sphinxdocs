aec88f644227eb826523003988ccf97854db4d1d
Thanh Le <thanhle@Thanhs-MacBook-Pro.local>
2016-10-06 18:55:09 -0700

    New generated docs

--- searchindex.js
+++ searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({docnames:["index"],envversion:50,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to Main Docs&#8217;s documentation!"],titleterms:{doc:0,document:0,indice:0,main:0,tabl:0,welcom:0}})
+Search.setIndex({docnames:["git","index"],envversion:50,filenames:["git.rst","index.rst"],objects:{},objnames:{},objtypes:{},terms:{content:1,index:1,modul:1,page:1,search:1,tbd:0},titles:["GIT","Welcome to Main Docs&#8217;s documentation!"],titleterms:{doc:1,document:1,git:0,indice:1,main:1,tabl:1,welcom:1}})
